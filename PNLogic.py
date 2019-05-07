909b459eafc23c68f09714663f9a17bf80cb3fb0
mackenzie.whitney <mackenzie.whitney@solium.com>
2019-05-07 14:52:08 -0600

    add first files to project

---
+++ PNLogic.py
@@ -0,0 +1,128 @@
+def repr_list(co_list):
+    starting_x = co_list[0][0]
+    ending_x = co_list[-1][0]
+    for x_pos in range(starting_x, ending_x+1):
+        print("")
+        for item in co_list:
+            if item[0] == x_pos:
+                print(item, end=' ')
+
+
+def show_optimal_play(p_list, co):
+    states = ['N', 'P']
+    x, y = list(co)
+
+    if co not in p_list:
+        i = 0
+    else:
+        i = 1
+
+    print((x, y), states[i])
+    if x > 0 and y > 0:
+        while x != y:
+            if x // y > 1:
+                if ((x % y) + y, y) in p_list:
+                    temp_x = (x % y) + y
+                    temp_y = y
+                else:
+                    temp_x = x % y
+                    temp_y = y
+            elif y // x > 1:
+                if (x, (y % x) + x) in p_list:
+                    temp_x = x
+                    temp_y = (y % x) + x
+                else:
+                    temp_x = x
+                    temp_y = y % x
+            elif x // y == 1 or y // x == 1:
+                if x > y:
+                    temp_x = x % y
+                    temp_y = y
+                elif x < y:
+                    temp_x = x
+                    temp_y = y % x
+            x = temp_x
+            y = temp_y
+            i = (i + 1) % len(states)
+            print((x, y), states[i])
+        i = (i + 1) % len(states)
+        print((x, 0), states[i])
+        i = (i + 1) % len(states)
+        print((0, 0), states[i])
+    if (x == 0 and y > 0) or (x > 0 and y == 0):
+        i = (i + 1) % len(states)
+        print((0, 0), states[i])
+
+
+def show_stat_block(p_list, co):
+    x, y = co
+    if x == y:
+        div_ratio = 0
+        full_move = (0, x)
+        alt_move = "N/A"
+    elif x == 0 or y == 0:
+        div_ratio = 0
+        full_move = (0, 0)
+        alt_move = "N/A"
+    else:
+        if x < y:
+            div_ratio = y // x
+            full_move = (x, y % x)
+            alt_move = (x, y % x + x)
+        else:
+            div_ratio = x // y
+            full_move = (x % y, y)
+            alt_move = (x % y + y, y)
+    print(f"coordinate is {co}")
+    print(f"full move is {full_move}")
+    if div_ratio > 1:
+       print(f"alt move is {alt_move}")
+    if div_ratio == 1 or div_ratio == 0:
+        if full_move in p_list:
+            print("the full move, which is mandatory, is a P-position")
+        else:
+            print("the full move, which is mandatory, is an N-position")
+    else:
+        if full_move in p_list and alt_move not in p_list:
+            print("the full move is the winning move")
+        elif full_move not in p_list and alt_move in p_list:
+            print("the alt move is the winning move")
+
+
+def generate_p(co, p_list):
+    x, y = co
+    if x == y:
+        return True
+    elif x == 0 or y == 0:
+        return False
+    else:
+        if x < y:
+            greater = y
+            lesser = x
+        else:
+            greater = x
+            lesser = y
+        div_ratio = greater // lesser
+        full_move = (greater % lesser, lesser) # note forced order. building p_list x < y
+
+        if div_ratio == 1:
+            if full_move in p_list:
+                return False
+            else:
+                return True
+        else:
+            # if div_ratio > 1, the winning move exists (full_move or alt_move) and is a N position
+            return False
+
+
+# necessary? especially if there is a way to know states P not by induction.
+# N_List not required. N is ~P
+def init(upper_bound):
+    p_list = []
+    for x in range(upper_bound+1):
+        for y in range(min(2*x, upper_bound+1)):
+            if generate_p((x, y), p_list) is True:
+                p_list.append((x, y))
+    return p_list
+
+
---
+++ UnitTests.py
@@ -0,0 +1,37 @@
+import unittest
+import PNLogic
+import io
+from contextlib import redirect_stdout
+
+
+P_List = PNLogic.init(100)
+
+
+class TestIsP(unittest.TestCase):
+    def test_same_number_is_P(self):
+        self.assertTrue((0, 0) in P_List)
+        self.assertTrue((7, 7) in P_List)
+
+
+class TestIsN(unittest.TestCase):
+    def test_exactly_one_zero_is_N(self):
+        self.assertTrue((0, 1) not in P_List)
+        self.assertTrue((7, 1) not in P_List)
+
+
+class TestOptimalPlay(unittest.TestCase):
+    def test_main_case(self):
+        f = io.StringIO()
+        with redirect_stdout(f):
+            PNLogic.show_optimal_play(P_List, (52, 15))
+        out = f.getvalue()
+        self.assertEqual(out,
+                         """(52, 15) N
+(22, 15) P
+(7, 15) N
+(7, 8) P
+(7, 1) N
+(1, 1) P
+(1, 0) N
+(0, 0) P
+""")
